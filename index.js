eb5686c82ab4537aa26a3d4e0c0132cc35772b5c
zeyti12a <daviraposo'124@gmAil.com>
2023-11-26 17:30:50 -0300

    instalaçao do handlebars

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express")
+const exphbs = require ("express-handlebars")
+
 
 const app = express() 
+
+app.engine('handlebars',exphbs.engine() )
+app.set('view engine' , 'handlebars')
+
 
 app.get('/' , (requisicao, resposta)=> {
     resposta.send("ola mundo")
