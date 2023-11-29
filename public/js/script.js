805a8c8bf3a5c406279a726a81df7957d7383047
zeyti12a <daviraposo'124@gmAil.com>
2023-11-28 21:07:38 -0300

    deixando a mudança de tema dinâmica

---
+++ public/js/script.js
@@ -0,0 +1,42 @@
+function alterartema(){
+    const tema = localStorage.getItem("tema")
+    const body= document.querySelector("body")
+    const button = document.querySelector(".tema-button")
+
+    if (tema) {
+        let novotema
+
+        if (tema === "light"){
+            novotema="dark"
+            button.innerHTML = `<img src="/imagens/sun-icon.png" alt="Icone de sol">`
+            body.classList.remove("light")
+            body.classList.add("dark")
+        } else{
+            novotema="light"
+            button.innerHTML = `<img src="/imagens/moon-icon.png" alt="Icone de lua">`
+            body.classList.remove("dark")
+            body.classList.add("light")
+        }
+
+        localStorage.setItem("tema", novotema)
+        return
+    }
+
+    localStorage.setItem("tema","dark")
+}
+
+function verifiicarTema(){
+    const tema = localStorage.getItem("tema")
+    const body = document.querySelector("body")
+    const button = document.querySelector(".tema-button")
+
+    if (tema === "dark") {
+        button.innerHTML = `<img src="/imagens/sun-icon.png" alt="Icone de sol">`
+        body.classList.add("dark")
+    }   else{
+        body.classList.add("light")
+        button.innerHTML = `<img src="/imagens/moon-icon.png" alt="Icone de lua">`
+    }
+}
+
+verifiicarTema()
